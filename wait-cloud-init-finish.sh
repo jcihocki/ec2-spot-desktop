bc05675125e60fce0bb36cd1932c784c48401521
jcihocki <cihocki@gmail.com>
2019-01-04 13:11:17 -0800

    Add region as param to phone home postback

--- wait-cloud-init-finish.sh
+++ wait-cloud-init-finish.sh
@@ -5,11 +5,14 @@ while ! test -d "/var/lib/cloud/instances/$1"; do
   sleep 10
 done
 
+EC2_AVAIL_ZONE=`curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone`
+EC2_REGION="`echo \"$EC2_AVAIL_ZONE\" | sed 's/[a-z]$//'`"
+
 # Fire off SQS msg and reboot
 while : ; do
 
   echo "Trying to say hello world to the desktop mgmt infra..."  > /dev/stderr
-  STATUSCODE=$(curl --silent --output /dev/null --write-out "%{http_code}" -X POST https://dev.remotewarriors.work/running-instances)
+  STATUSCODE=$(curl --silent --output /dev/null --write-out "%{http_code}" -d region="${EC2_REGION}" -X POST https://dev.remotewarriors.work/running-instances)
   if [ $STATUSCODE -eq 204 ]; then break; fi
   echo "Failed, http status is $STATUSCODE" > /dev/stderr
   sleep 15
