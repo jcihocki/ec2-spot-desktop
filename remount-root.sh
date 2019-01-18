8a69f7510c246ee76c8d4eef72be53f959520341
John Cihocki <cihocki@gmail.com>
2019-01-18 12:10:50 -0800

    Branching behavior with fallbacks for m5 and r5 instances in /sbin/init hack

--- remount-root.sh
+++ remount-root.sh
@@ -6,6 +6,7 @@ else
 fi
 
 DEVICE=/dev/$(ls -l /dev/disk/by-label|grep permaroot|cut -d / -f 3)
+INSTANCE_TYPE=$(curl http://169.254.169.254/latest/meta-data/instance-type)
 echo "Will use ${DEVICE} as persistent chroot"
 umount ${DEVICE}
 NEWMNT=/permaroot
@@ -15,6 +16,7 @@ e2fsck $DEVICE -f -y
 # e2label $DEVICE permaroot
 tune2fs $DEVICE -U `uuidgen`
 mkdir -p $NEWMNT
+
 
 mount $DEVICE $NEWMNT
 if [ ! -f $NEWMNT/etc/openvpn/server.key ]; then 
@@ -28,15 +30,47 @@ if [ ! -f $NEWMNT/etc/openvpn/server.key ]; then
 	cp client.ovpn $NEWMNT/home/ubuntu/
 fi
 cp /etc/hostname $NEWMNT/etc/hostname
+mkdir -p $NEWMNT/$OLDMNT
 umount $DEVICE
 
 #
 # point of no return... 
 # modify /sbin/init on the ephemeral volume to chain-load from the persistent EBS volume, and then reboot.
 #
-if [ -L "/sbin/init" ]
-then
-   mv /sbin/init /sbin/init.backup
+mv /sbin/init /sbin/init.backup
+if [[ $INSTANCE_TYPE == m5* || $INSTANCE_TYPE == r5* ]]; then
+  
+  
+  
+   cat >/sbin/init <<EOF
+#!/bin/sh
+/bin/mount $DEVICE $NEWMNT 
+echo "JOHNNY Path is $PATH" > /dev/kmsg
+echo "JOHNNY Mount: $(which mount) Unshare: $(which unshare) chroot: $(which chroot) pivot_root: $(which pivot_root)" > /dev/kmsg
+cd $NEWMNT
+
+if [ -f /bin/unshare ]; then 
+  /bin/unshare -m
+  /sbin/pivot_root . ./$OLDMNT
+  PIVOT_STATUS=$?
+  echo "JOHNNY Pivot root status: \$PIVOT_STATUS Mount status: $(mount)" > /dev/kmsg
+  if [ "\$PIVOT_STATUS" = "0" ]; then 
+    for dir in /dev /proc /sys /run; do
+       echo "JOHNNY Moving mounted file system ${OLDMNT}\${dir} to \$dir." > /dev/kmsg
+       mount --move ./${OLDMNT}\${dir} \${dir}
+    done
+
+    exec chroot . /sbin/init
+  fi
+fi
+echo "JOHNNY had to bail out of chroot for some reason. Check kernel logs" > /dev/kmsg
+exec /sbin/init.backup
+EOF
+  
+else
+  
+
+
    cat >/sbin/init <<EOF
 #!/bin/sh
 mount $DEVICE $NEWMNT
@@ -47,7 +81,6 @@ mount $DEVICE $NEWMNT
 # TODO add any ssh pubkeys here   
    
 cd $NEWMNT
-#unshare -m
 pivot_root . ./$OLDMNT
    
 for dir in /dev /proc /sys /run; do
@@ -56,7 +89,7 @@ for dir in /dev /proc /sys /run; do
 done
 exec chroot . /sbin/init
 EOF
-   chmod +x /sbin/init
 fi
+chmod +x /sbin/init
 
 bash wait-cloud-init-finish.sh $(curl http://169.254.169.254/latest/meta-data/instance-id) &
