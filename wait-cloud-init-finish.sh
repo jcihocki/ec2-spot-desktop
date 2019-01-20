a634fe4b062484dc1d1a6caf18e983209e593696
Johnny C <0@johnnyc.lol>
2019-01-20 17:09:01 -0500

    Tag passwd from main script

--- wait-cloud-init-finish.sh
+++ wait-cloud-init-finish.sh
@@ -7,11 +7,4 @@ while ! test -d "/var/lib/cloud/instances/$INSTANCE_ID"; do
   sleep 10
 done
 
-PASSWD=$(/usr/bin/node generate-passwd.sh)
-echo "$INSTANCE_ID, $ZONE, $REGION, $PASSWD"
-
-aws ec2 create-tags --resources $INSTANCE_ID --tags "Key=Password,Value=$PASSWD" --region $REGION
-yes $PASSWD | passwd ubuntu
-cp /etc/shadow /permaroot/etc/shadow
-history -c
 
