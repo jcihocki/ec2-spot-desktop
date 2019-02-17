2722b69b929b5b93ef7481d5c594bf17e7846ffe
Johnny <cihocki@gmail.com>
2019-02-17 19:59:51 +0000

    Total fail committing last version

--- remount-root.sh
+++ remount-root.sh
@@ -1,8 +1,23 @@
 ROOT_LABEL="$(findmnt / -o label -n)"
-if [ $ROOT_LABEL == "permaroot" ]; then 
-	exit 0 
-else 
-	echo "Root label is $ROOT_LABEL which means we need to set up and reboot for chroot" 
+if [ $ROOT_LABEL == "permaroot" ]; then
+	cp /old-root/etc/hostname /etc/hostname
+	if [ ! -f $NEWMNT/etc/openvpn/server.key ]; then
+
+		echo "Doing one time OpenVPN setup..."
+
+		# Generate new openvpn keys and certs, this is a new desktop
+		git clone https://github.com/jcihocki/openvpn-server-conf.git
+		cd openvpn-server-conf
+		bash server-setup.sh
+
+		cp client.ovpn $NEWMNT/home/ubuntu/
+		cd ..
+	fi
+
+	bash wait-cloud-init-finish.sh $(curl http://169.254.169.254/latest/meta-data/instance-id) $(cat /home/ubuntu/client.ovpn)
+	exit 0
+else
+	echo "Root label is $ROOT_LABEL which means we need to set up and reboot for chroot"
 fi
 
 DEVICE=/dev/$(ls -l /dev/disk/by-label|grep permaroot|cut -d / -f 3)
@@ -17,22 +32,12 @@ mkdir -p $NEWMNT
 
 
 mount $DEVICE $NEWMNT
-# if [ ! -f $NEWMNT/etc/openvpn/server.key ]; then
-#   # Generate new openvpn keys and certs, this is a new desktop
-#   git clone https://github.com/jcihocki/openvpn-server-conf.git
-#         cd openvpn-server-conf
-#   bash server-setup.sh
-#
-#   cd ..
-#   cp /etc/openvpn/server.conf /etc/openvpn/ca.crt /etc/openvpn/ca.key /etc/openvpn/ta.key /etc/openvpn/crl.pem /etc/openvpn/server.crt /etc/openvpn/server.key /etc/openvpn/dh.key $NEWMNT/etc/openvpn/
-#   cp client.ovpn $NEWMNT/home/ubuntu/
-# fi
 cp /etc/hostname $NEWMNT/etc/hostname
 mkdir -p $NEWMNT/$OLDMNT
 umount $DEVICE
 
 #
-# point of no return... 
+# point of no return...
 # modify /sbin/init on the ephemeral volume to chain-load from the persistent EBS volume, and then reboot.
 #
 mv /sbin/init /sbin/init.backup
@@ -42,7 +47,7 @@ cat >/sbin/init <<EOF
 mount UUID=$DEVICE_UUID /permaroot
 cd $NEWMNT
 
-pivot_root . ./$OLDMNT 
+pivot_root . ./$OLDMNT
 
 for dir in /dev /proc /sys /run; do
    echo "JOHNNY Moving mounted file system ${OLDMNT}\${dir} to \$dir." > /dev/kmsg
@@ -55,4 +60,4 @@ EOF
 
 chmod +x /sbin/init
 
-bash wait-cloud-init-finish.sh $(curl http://169.254.169.254/latest/meta-data/instance-id) &
+bash wait-cloud-init-finish.sh $(curl http://169.254.169.254/latest/meta-data/instance-id) && nohup bash -c "sleep 2 && reboot" &
